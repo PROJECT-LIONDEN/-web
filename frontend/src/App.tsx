7b741aedea276ef7f649441e522a1c31a93542b3
Chad Thomas <chad.thomas@neudesic.com>
2021-04-13 10:51:06 -0400

    fixes typo in find syntax

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,79 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { Route, BrowserRouter as Router, Switch, Link} from 'react-router-dom';
-import Product from './Products/Products';
-import Home from './Home/Home';
-import Orders from './Orders/Orders';
+import React from "react";
+import "./App.css";
+import { Route, BrowserRouter as Router, Switch, Link } from "react-router-dom";
+import Product from "./Products/Products";
+import Home from "./Home/Home";
+import Orders from "./Orders/Orders";
+import OrderDetail from "./Orders/OrderDetails";
 
 function App() {
   const openMenu = () => {
     document.querySelector(".sidebar")?.classList.add("open");
-  }
+  };
   const closeMenu = () => {
     document.querySelector(".sidebar")?.classList.remove("open");
-  }
- return ( 
+  };
+  return (
     <Router>
-    <div className="grid-container">
+      <div className="grid-container">
         <header className="header">
-            <div className="brand">
-                <button onclick="openMenu()">&#9776;</button>
-                <a href="index.html">PROJECT LIONDEN</a>
-            </div>
-            <div className="header-links">
-                 <Link to="/">Home</Link>
-                 <Link to="/catalog">Catalog</Link>
-                 <Link to="/orders">Orders</Link>
-             </div>
+          <div className="brand">
+            <button onClick={openMenu}>&#9776;</button>
+            <a href="index.html">PROJECT LIONDEN</a>
+          </div>
+          <div className="header-links">
+            <Link to="/">Home</Link>
+            <Link to="/catalog">Catalog</Link>
+            <Link to="/orders">Orders</Link>
+          </div>
         </header>
-    <head>
-        <link rel="stylesheet" href="style.css">
-        <title>PROJECT LIONDEN</title>
-    </head>
-         <body>
-             <div>
-                <aside className="sidebar">
-                    <h3>Shopping Categories</h3>
-                    <button className="sidebar-close-button" onclick ="closeMenu()">x</button>
-                    <ul>
-                        <li>
-                            <a href="index.html">Pants</a>
-                        </li>
-                        <li>
-                            <a href= "index.html">Shirts</a>
-                        </li>
-                    </ul>
-                </aside>
-                <main>
-                <main className="main">
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/catalog">
-            <Product />
-          </Route>
-          <Route path="/orders">
+        <aside className="sidebar">
+          <h3>Shopping Categories</h3>
+          <button className="sidebar-close-button" onClick={closeMenu}>
+            x
+          </button>
+          <ul>
+            <li>
+              <a href="index.html">Pants</a>
+            </li>
+            <li>
+              <a href="index.html">Shirts</a>
+            </li>
+          </ul>
+        </aside>
+        <main className="main">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/catalog">
+              <Product />
+            </Route>
+            <Route path="/orders">
               <Orders />
-          </Route>
-          <Route path="/order/:id">
-                <OrderDetail />
-          </Route>
-        </Switch>
+            </Route>
+            <Route path="/order/:id">
+              <OrderDetail />
+            </Route>
+          </Switch>
         </main>
-                <footer className= "footer">
-                   &copy; 2021 PROJECT LIONDEN
-                </footer>
-             </div>
-             <div className="content">
-                 <ul className="products">
-                 
-                 </ul>
-             </div>
-         </body>
-    </div>
-</Router>
+        <footer className="footer">&copy; 2021 PROJECT LIONDEN</footer>
+      </div>
+    </Router>
   );
 }
 
